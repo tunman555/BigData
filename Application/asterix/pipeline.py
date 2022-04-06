1074fef92a950ea56b0336ee9032f6da69a1233f
tunman555 <punjaburi_tun@hotmail.com>
2022-04-06 15:35:23 +0000

    Update pipeline.py

--- Application/asterix/pipeline.py
+++ Application/asterix/pipeline.py
@@ -1,45 +1,39 @@
 import glob
 import schedule
 import time
-import os 
-import subprocess
+import os
+import shutil 
 
 def get_latest(path):
-	list_of_files = glob.glob(path + '/*.gz')
-	if len(list_of_files) != 0:
-		return max(list_of_files,key = os.path.getctime)
-	else :
-		return None
+        list_of_files = glob.glob(path + '/*.gz')
+        if len(list_of_files) != 0:
+                return max(list_of_files,key = os.path.getctime)
+        else :
+                return None
 cat = ['RADAR','ADSB','CAT62']
 
+wd = "/home/system/asterix_collection/"
+target_dir = '/home/ftper/data/'
+
 def job():
-	for i in cat:
-		site_addr = glob.glob("../data/" + i + "/*")
-		for j in site_addr:
-			print(j)
-		
-			latest_file = get_latest(j)
-			if latest_file != None :
-		
-				src_type = latest_file.split('/')[2].split('\\')[0]
-		
-				if src_type == 'RADAR':
-					subprocess.call("python ./asterix_decode_cat48.py " + latest_file,shell=True)
-		
-				elif src_type == 'ADSB':
-                        		subprocess.call("python ./asterix_decode_cat21.py " + latest_file,shell=True)
-				elif src_type == 'CAT62':
-					if latest_file.split('/')[3] =='ACC62':
-                        			subprocess.call("python ./asterix_decode_ACC62.py " + latest_file,shell=True)
-					elif latest_file.split('/')[3] =='ADSB62':
-                                                subprocess.call("python ./asterix_decode_ADSB62.py " + latest_file,shell=True)
-					elif latest_file.split('/')[3] =='RAD62':
-                                                subprocess.call("python ./asterix_decode_RAD62.py " + latest_file,shell=True)
+        for i in cat:
+                site_addr = glob.glob(wd + "data/" + i + "/*")
+                for j in site_addr:
+                        latest_file = get_latest(j)
 
-			else : pass
+                        if latest_file != None :
+                                src_type = latest_file.split('/')[5]
+                                main = latest_file.split('/')[6][-1]
+                                if src_type == 'RADAR' and main=='1':
+                                        shutil.move(latest_file, target_dir + '/RADAR/' + j)
+                                elif src_type == 'ADSB':
+                                        shutil.move(latest_file, target_dir + '/ADSB/' + j)
+                                elif src_type == 'CAT62':
+                                        if latest_file.split('/')[6] =='ACC62':
+                                                shutil.move(latest_file, target_dir + '/CAT62/' + j)
 
-schedule.every().hour.at(":04").do(job)
+                        else : pass
 
+schedule.every().hour.at(":00").do(job)
 while True:
-	schedule.run_pending()
-	time.sleep(1)
+        schedule.run_pending()
